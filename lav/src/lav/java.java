bc0b21184df5f7fb7488525221e06f03a1cc0425
lavanyagr83 <plavanyag@gmail.com>
2022-06-17 14:01:18 +0530

    java program

---
+++ lav/src/lav/java.java
@@ -0,0 +1,16 @@
+package lav;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f;
+		float c=12.5f;
+		f=((c*9)/5)+32;
+		
+		
+		System.out.println(f);
+
+	}
+
+}
